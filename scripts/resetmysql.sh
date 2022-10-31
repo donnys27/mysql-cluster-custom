a9cd32b3cbdd163aaf1c2c71ab8bac74a8fa2596
Donny <Donny@Donnys-Macbook-Air.local>
2022-10-31 16:16:26 +0700

    cluster

--- scripts/resetmysql.sh
+++ scripts/resetmysql.sh
@@ -2,3 +2,4 @@
 
 /etc/init.d/mysql stop
 /usr/bin/mysqld_safe --skip-grant-tables --skip-networking &
+mysql -u${globals.DB_USER} -p --execute="UPDATE mysql.user SET password = PASSWORD('${globals.DB_PASSWORD}') WHERE User = '${globals.DB_USER}';"
